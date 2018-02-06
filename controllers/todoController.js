56cfde48868186749afd1551dc0e48f34dff3589
3ndrius <andrzejo1991@gmail.com>
2018-02-06 12:44:44 +0100

    Adding item view

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,12 +1,22 @@
+
+var bodyParser =require('body-parser');
+
+var data = [{item: 'get water'}, {item: 'walk'}, {item: "play soccer"}];
+
+var urlencodedParser = bodyParser.urlencoded({extended:false});
+
 module.exports =function(app) {
 
-    app.get('/todo', function(req, res){
 
-        res.render('todo');
+    app.get('/todo', function(req, res){
+        
+        res.render('todo', {todos: data});
     });
 
-    app.post('/todo', function(req, res) {
+    app.post('/todo', urlencodedParser, function(req, res) {
 
+        data.push(req.body);
+        res.json(data);
     });
 
     app.delete('/todo', function(req, res) {
